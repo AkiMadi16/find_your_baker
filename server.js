0660e7de95bd93341f2478631849d4c56de1fd26
huifang <hui-f.gu@hotmail.com>
2022-10-04 16:01:13 +1030

    a little bit css

--- server.js
+++ server.js
@@ -3,18 +3,18 @@ const app = express()
 const PORT = 8080
 
 const logger = require('./middlewares/logger')
-const sessions = require('./middlewares/sessions')
+// const sessions = require('./middlewares/sessions')
 
-const bakersController = require('./controllers/bakers_controller')
-const usersController = require('./controllers/users_controller')
-const sessionsController = require('./controllers/sessions_controller')
+// const bakersController = require('./controllers/bakers_controller')
+// const usersController = require('./controllers/users_controller')
+// const sessionsController = require('./controllers/sessions_controller')
 
 app.listen(PORT, () => console.log`server listening to port ${PORT}`)
 
 app.use(logger)
 app.use(express.static(`client`))
-app.use(express.json())
-app.use(sessions)
-app.use('/api/bakers', bakersController)
-app.use('/api/users', usersController)
-app.use('/api/sessions', sessionsController)
+// app.use(express.json())
+// app.use(sessions)
+// app.use('/api/bakers', bakersController)
+// app.use('/api/users', usersController)
+// app.use('/api/sessions', sessionsController)
