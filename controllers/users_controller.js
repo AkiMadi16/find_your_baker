b4441fd75b7770943a34d802fef9623c94655868
AkiMadie16 <madie86@gmail.com>
2022-10-04 21:57:42 +1030

    error message done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -8,14 +8,25 @@ const User = require('../models/user')
 
 // routes
 router.post('/', (req, res) => {
-    const { name, email, password } = req.body
-  
-    // using bcrypt to create password digest
-    const passwordDigest = bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
-  
-    User
-      .create(name, email, passwordDigest)
-      .then(userEmail => res.json(userEmail))
-  })
+  const {userName, email, password, confirmPassword} = req.body
+ 
+  if (password === confirmPassword) {
+      const passwordDigest = bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
+
+      User 
+          .findByEmail(email) 
+          .then(user => {
+            if (user) {
+              res.json({ error: 'This email is already associated with an account!' })
+            } else {
+              User 
+                .create(userName, email, passwordDigest)
+                .then(email => res.json(email))
+            }   
+      }) 
+  } else {
+    res.json({ error: 'Password does not match!' })
+  }
+})
 
   module.exports = router
