d820bc9da27c3c0b60d5ca125379aa362a478bc4
huifang <hui-f.gu@hotmail.com>
2022-10-04 18:42:31 +1030

    for fetch

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const PORT = 8080
 const logger = require('./middlewares/logger')
 const sessions = require('./middlewares/sessions')
 
+<<<<<<< HEAD
 const bakersController = require('./controllers/bakers_controller')
+=======
+// const bakersController = require('./controllers/bakers_controller')
+>>>>>>> for fetch
 const usersController = require('./controllers/users_controller')
 const sessionsController = require('./controllers/sessions_controller')
 
@@ -15,6 +19,12 @@ app.use(logger)
 app.use(express.static(`client`))
 app.use(express.json())
 app.use(sessions)
+<<<<<<< HEAD
 // app.use('/api/baker', bakersController)
 app.use('/api/users', usersController)
 // app.use('/api/sessions', sessionsController)
+=======
+// app.use('/api/bakers', bakersController)
+app.use('/api/users', usersController)
+app.use('/api/sessions', sessionsController)
+>>>>>>> for fetch
