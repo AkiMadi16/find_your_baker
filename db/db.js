245373fbebcf979c86b0a29ba7939ac339f0bbf7
huifang <hui-f.gu@hotmail.com>
2022-10-05 15:04:55 +1030

    find your baker mvp

--- db/db.js
+++ db/db.js
@@ -1,8 +1,24 @@
 const pg = require('pg')
 
-const db = new pg.Pool({
-    database: 'baker_collective',
-    password: '1111'
-})
+let db;
+if (process.env.DATABASE_URL) {
+  db = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  })
+} else {
+  if (process.env.DB_PASSWORD) {
+    db = new pg.Pool({
+      database:  'baker_collective',
+      password: process.env.DB_PASSWORD
+    })
+  } else {
+    db = new pg.Pool({
+      database: localDbName
+    })
+  }
+}
 
 module.exports = db 
