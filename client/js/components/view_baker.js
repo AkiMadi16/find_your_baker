1fd66aec509b30701d48ba26a4165ab0db40ee14
AkiMadie16 <madie86@gmail.com>
2022-10-05 22:31:08 +1030

    updating changes

--- client/js/components/view_baker.js
+++ client/js/components/view_baker.js
@@ -2,7 +2,7 @@ function showBakerDetails(bakerId) {
 
   const baker = state.bakers.find(baker => baker.id === bakerId)
 
-  console.log(baker)
+  // console.log(baker)
   const BingMapsKey = "BING_API_KEY"
 
   const locationFinder = `http://dev.virtualearth.net/REST/v1/Locations?CountryRegion=AU&addressLine=${baker.address}&key=${BingMapsKey}`
@@ -10,7 +10,7 @@ function showBakerDetails(bakerId) {
   fetch(locationFinder)
     .then(res => res.json() )
     .then(res => {
-      console.log(res);
+      // console.log(res);
       // get lat, long from maps api
       const geo = res.resourceSets[0].resources[0].point.coordinates.join(',');
 
