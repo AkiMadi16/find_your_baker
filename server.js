1345afe4a742e23f2457e1504193a538ea851703
AkiMadie16 <madie86@gmail.com>
2022-10-05 16:09:14 +1030

    posssible server file conflict

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3001;
+<<<<<<< HEAD
 
+=======
+>>>>>>> e938309 (posssible server file conflict)
 
 const logger = require('./middlewares/logger')
 const sessions = require('./middlewares/sessions')
