aa6405f2936cad57cb03741be8185cc0f2200c4f
huifang <hui-f.gu@hotmail.com>
2022-12-10 23:45:52 -0800

    add star rating in personal review panel

--- client/js/components/each_user_reviews.js
+++ client/js/components/each_user_reviews.js
@@ -44,20 +44,27 @@ function addBakerNameToReviews () {
 
 function userReviews() {
   console.log(reviews.reviews)
-  return reviews.reviews.map(review =>  
-    
-    ` 
+  return reviews.reviews.map(review =>{
+    const starTotal = 5;
+    const starPercentage = ((review.rating)/ starTotal) * 100;
+    const starPercentageRounded = `${(Math.round(starPercentage / 10) * 10)}%`;
+    return ` 
     <div class='baker card' data-id='${review.id}' data-review='${review.review}' data-rating='${review.rating}'>
-    <div class="card-body">
-        <h5 class="card-title">${review.bakerName}</h5>
-        <p class="card-text">${review.review}</p>
-        <a onClick = "deleteAReview(event)" class="card-link">Delete</a>
-        <a onClick = "renderUpdateReview(event)" class="card-link">Update</a>
+      <div class="card-body">
+        <h5 class="card-title">${review.user_name}</h5>
+        <div class="d-inline-flex">
+          <div class="stars-outer">
+            <div class="stars-inner" style="width:${starPercentageRounded};"></div>
+          </div>
+          <a class="attribution" href="http://fontawesome.io/"><i class="fa fa-font-awesome"></i></a>
+          <p class="ms-3">${review.rating}</p>
+        </div>
+          <p class="card-text">${review.review}</p>
+          <a onClick = "deleteAReview(event)" class="card-link">Delete</a>
+          <a onClick = "renderUpdateReview(event)" class="card-link">Update</a>
+      </div>
     </div>
-  </div>
-  `).join('')
-
-
+  `}).join('')
 }
 
 function deleteAReview(event) {
